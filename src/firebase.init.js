d287f6bc8158a4bd5e1dcf5a044eaa8023dd86f4
numansaady <numansaady@gmail.com>
2022-05-31 14:54:59 +0600

    .env.local file added with firebase config

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCGCUFsgZQeX7kSMOnaGj5NFy4Tcj3oTVM",
-  authDomain: "munshi-digital-sign.firebaseapp.com",
-  projectId: "munshi-digital-sign",
-  storageBucket: "munshi-digital-sign.appspot.com",
-  messagingSenderId: "493830668326",
-  appId: "1:493830668326:web:f7324441b292907f2226e7",
+  apiKey:process.env.REACT_APP_API_KEY,
+  authDomain:process.env.REACT_APP_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
